ff0f696a0915e7164c8059148cfa8da0fcfd4649
duyhong <duyhong@berkeley.edu>
2017-11-08 14:44:55 -0800

    blocipedia-checkpoint2-messy: not finished

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
 if Rails.env.development? || Rails.env.production?
     ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
-      address:        'smtp.sendgrid.net',
-      port:           '2525',
+      address:         'smtp.gmail.com',  # 'smtp.sendgrid.net',
+      port:           587,  # '2525',
       authentication: :plain,
-      user_name:      ENV['SENDGRID_USERNAME'],
-      password:       ENV['SENDGRID_PASSWORD'],
+      user_name:      ENV["GMAIL_USERNAME"],  # ENV['SENDGRID_USERNAME'],
+      password:       ENV["GMAIL_PASSWORD"],  # ENV['SENDGRID_PASSWORD'],
       domain:         'heroku.com',
       enable_starttls_auto: true
     }
