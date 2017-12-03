0aea7b94c992a3460d2d2a06ec2ad675746497c7
duyhong <duyhong@berkeley.edu>
2017-12-03 14:30:48 -0800

    blocipedia-checkpoint6: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do |index|
+  User.create!(email: Faker::Internet.email, password: Faker::Internet.password)
+  # Wiki.create!(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, user_id: user.sample.id)
+end
+
+User.all.each do |user|
+  Wiki.create!(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, user_id: user.id)
+end
+
+p "Created #{User.count} users"
+p "Created #{Wiki.count} wikis"
