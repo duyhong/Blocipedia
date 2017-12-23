c184a3480b3835baca4c3372a609d38df7933804
duyhong <duyhong@berkeley.edu>
2017-12-23 14:22:27 -0800

    blocidepia-checkpoint8: completed: - added data: {confirm: 'Your private wikis will become public after you downgrade your account. Are you sure?'} in devise\registration\edit.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
       wiki.update(private: !wiki.private?)
     end
     current_user.standard!
-    flash[:notice] = "Your private wikis will become public after you downgrade your account."
     redirect_to edit_user_registration_path
   end
 end
